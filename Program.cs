5e2791a7c5a035f51b59b48807dfc633b7c0d636
Victor Matheus <matheuscorreaosu@gmail.com>
2022-11-22 22:16:42 -0300

    done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone tijolo = new Nokia("91981765608", "Nokia 5510", "111111111", 128);
+Smartphone sj = new Iphone("1283121312", "Iphone 13", "2222222222", 256);
+
+tijolo.Ligar();
+tijolo.InstalarAplicativo("The Sims");
+
+
+sj.ReceberLigacao();
+sj.InstalarAplicativo("Pou");
